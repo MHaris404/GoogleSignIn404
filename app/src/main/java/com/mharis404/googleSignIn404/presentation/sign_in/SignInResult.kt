9fd56b9a94713673d7d82e37be6720ce9ed2f7bf
Muhammad Haris <muhammad.haris@puresquare.com>
2025-04-06 22:30:13 +0500

    first commit (old code)

---
+++ app/src/main/java/com/mharis404/googleSignIn404/presentation/sign_in/SignInResult.kt
@@ -0,0 +1,12 @@
+package com.mharis404.googleSignIn404.presentation.sign_in
+
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilePictureUrl: String?
+)
---
+++ app/src/main/java/com/mharis404/googleSignIn404/presentation/sign_in/SignInState.kt
@@ -0,0 +1,6 @@
+package com.mharis404.googleSignIn404.presentation.sign_in
+
+data class SignInState(
+    val isSignInSuccessful: Boolean = false,
+    val signInError: String? = null
+)
